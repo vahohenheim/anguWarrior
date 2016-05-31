b6a70622887e092b98846fad940ab8bde8e7b951
Louis-Marie Marcadon <louis.marcadon@gmail.com>
2016-05-31 14:55:36 +0200

    Correction du service RestAngular avec des exemples

--- js/service.js
+++ js/service.js
@@ -1,5 +1,23 @@
-angular.module('myApp').factory('ServiceRest', function(Restangular) {
-  return Restangular.withConfig(function(RestangularConfigurer) {
+angular.module('myApp').factory('ServiceRest', ['Restangular', function(Restangular){
+
     RestangularConfigurer.setBaseUrl('http://localhost:3000/');
-  });
-});
+    // this is service object with list of methods in it
+    // this object will be used by controller
+    var service = {
+        getExamples: getExamples,
+        getExample: getExample
+    };
+
+    // get examples from server by using Restangular
+    function getExamples(){
+        return Restangular.one('posts').getList();
+    }
+
+    // get example with given id from server by using Restangular
+    function getExample(exampleId){
+        return Restangular.one('/posts/'+exampleId).get();
+    }
+
+    return service;
+
+}]);
